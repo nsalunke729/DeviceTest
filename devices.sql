119371345d19c50ccf8a4cb472c04d0cd1d938d8
Niranjan <nsalunke729@gmail.com>
2025-03-21 12:12:56 +0000

    changes in sql script

--- devices.sql
+++ devices.sql
@@ -1,9 +1,22 @@
-CREATE TABLE devices (
-    id SERIAL PRIMARY KEY,
-    model VARCHAR(255),
-    vendor VARCHAR(255),
-    os_name VARCHAR(255),
-    os_version VARCHAR(10),
-    browser_name VARCHAR(255),
-    primary_hardware_type VARCHAR(50)
-);
+-- Table: public.devices
+
+-- DROP TABLE IF EXISTS public.devices;
+
+CREATE TABLE IF NOT EXISTS public.devices
+(
+    id integer NOT NULL DEFAULT nextval('devices_id_seq'::regclass),
+    model character varying(255) COLLATE pg_catalog."default",
+    vendor character varying(255) COLLATE pg_catalog."default",
+    "osName" character varying(255) COLLATE pg_catalog."default",
+    "osVersion" character varying(10) COLLATE pg_catalog."default",
+    "browserName" character varying(255) COLLATE pg_catalog."default",
+    "primaryHardwareType" character varying(50) COLLATE pg_catalog."default",
+    "createdAt" timestamp without time zone,
+    "updatedAt" timestamp without time zone,
+    CONSTRAINT devices_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.devices
+    OWNER to postgres;
